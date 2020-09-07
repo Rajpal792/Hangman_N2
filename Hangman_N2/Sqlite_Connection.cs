9e7ac4933bb82cbf51b560e7a01675c9648e7b2e
tanveer99 <tanveerpannu99@gmail.com>
2020-09-07 21:02:30 +1200

    Add project files.

---
+++ Hangman_N2/Sqlite_Connection.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Hangman_N2.Resources;
+using SQLite;
+
+namespace Hangman_N2
+{
+  public class Sqlite_Connection
+    {
+        private string data_base_path { get; set; }
+
+        private SQLiteConnection db { get; set; }
+
+
+
+
+
+        public Sqlite_Connection()
+        {
+
+            string data_base_path =
+                Path.Combine(System.Environment.GetFolderPath
+                    (System.Environment.SpecialFolder.Personal), "HangmanDB.sqlite");
+
+            db = new SQLiteConnection(data_base_path);
+
+            db.CreateTable<Score_Table>();
+        }
+
+
+
+
+        public List<Score_Table> ViewAll()
+        {
+            try
+            {
+                ;
+                return db.Query<Score_Table>("select *  from Score_Table  ORDER BY Score DESC");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error:" + e.Message);
+                return null;
+            }
+        }
+
+
+
+
+
+
+
+        public string UpdateScore(int id, string name, int score)
+        {
+
+
+            try
+            {
+                string data_base_path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),
+                    "HangmanDB.sqlite");
+                var db = new SQLiteConnection(data_base_path);
+                var item = new Score_Table();
+
+                item.Id = id;
+                item.Name = name;
+                item.Score = score;
+
+                db.Update(item);
+                return "Record Updated...";
+            }
+            catch (Exception ex)
+            {
+                return "Error : " + ex.Message;
+            }
+
+        }
+
+
+
+        public string InsertNewPlayer(string name, int score)
+        {
+
+
+            try
+            {
+                string data_base_path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),
+                    "HangmanDB.sqlite");
+                var db = new SQLiteConnection(data_base_path);
+                var item = new Score_Table();
+                item.Name = name;
+                item.Score = score;
+
+                db.Insert(item);
+                return "You have been added to the database";
+            }
+            catch (Exception ex)
+            {
+                return "Error : " + ex.Message;
+            }
+
+        }
+
+
+        public string DeletePlayer(int id)
+        {
+
+            try
+            {
+                string data_base_path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),
+                    "HangmanDB.sqlite");
+                var db = new SQLiteConnection(data_base_path);
+                var item = new Score_Table();
+                item.Id = id;
+
+
+                db.Delete(item);
+                return "You have been added to the database";
+            }
+            catch (Exception ex)
+            {
+                return "Error : " + ex.Message;
+            }
+
+
+        }
+
+    }
+}
---
+++ Hangman_N2/View_All_Activity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Hangman_N2.Resources;
+using SQLite;
+
+namespace Hangman_N2
+{
+    [Activity(Label = "View_All_Activity")]
+    public class View_All_Activity : Activity
+    {
+        private Button btn_play_again;
+        List<Score_Table> List_Max;
+        public Sqlite_Connection MyCon;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.View_All_Score);
+          btn_play_again = FindViewById<Button>(Resource.Id.btn_play_again);
+           btn_play_again.Click += btn_play_again_Click;
+
+           MyCon = new Sqlite_Connection();
+           List_Max = MyCon.ViewAll();
+
+           ListView List_View_All = FindViewById<ListView>(Resource.Id.List_View_All);
+            List_View_All.Adapter = new Adpter_Spiner(this, List_Max);
+
+
+
+
+        }
+
+        private void btn_play_again_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
